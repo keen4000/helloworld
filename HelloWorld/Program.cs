fb57f8714c15a440cc4306dfd7aacd34ed0dc1ef
keen4000 <me@straxler.ch>
2017-05-05 21:55:22 +0200

    Aks users for name and greet them accordingly

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello world");
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
             Console.ReadLine();
         }
     }
